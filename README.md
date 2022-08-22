2635368681cbdb84a6dbce079d44dfe7d76b918f
Amado Tejada <44622125+amadotejada@users.noreply.github.com>
2022-08-22 11:33:41 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# TapmacOS
-Tap is a tool that encourages users to update macOS without forcefully interrupting their workflow
+# Tap - macOS Updates
+##### Written by [Amado Tejada](https://www.linkedin.com/in/amadotejada/)
+
+
+## Introduction
+macOS Ventura introduces useful new [features](https://developer.apple.com/videos/play/wwdc2022/10045/) to manage macOS updates via an MDM solution. Devices that not on macOS Ventura have limited functionality when it comes to managing macOS updates via MDM, and other solutions are too disruptive for some enterprise environments.
+
+*Tap* is a tool that encourages users to update macOS without aggressively interrupting user workflow.
+
+## Screenshots
+
+With Custom Icon
+| Defer Available  | No Deferrals |
+|:-----:|:-----:|
+| <img src="./screenshots/deff_custom.png" width="100%"> | <img src="./screenshots/nodeff_custom.png" width="100%"> |
+
+With Default Software Update Icon
+| Defer Available  | No Deferrals |
+|:-----:|:-----:|
+| <img src="./screenshots/deff_default.png" width="100%"> | <img src="./screenshots/nodeff_default.png" width="100%"> |
+
+## Requirements
+-  Jamf Pro
+-  Administrator privileges
+
+## Features
+* run with bash, no dependencies
+* users can defer updates 3x to the next run
+* no Jamf Pro API account required
+* supports Intel and M1 devices
+    * using system preferences to update
+    * not using the deprecating `softwareupdate` command
+* simply configurable
+    * targetos - which version to look for
+    * how many times user can defer updates
+    * custom image to show on the GUI dialog
+    * description of GUI dialog
+
+
+## Initial Jamf Setup
+1. Upload `tap_updates.sh` to the Jamf Pro as a script
+2. Create Smart group for endpoints that you want to update
+    - Computers not on the *Tap* targeted version
+3. Set *Tap* variables in `tap_updates.sh` as needed
+    - `macosname` - the name of the macOS version to target e.g. "Monterey"
+    - `targetosverion` - which version to check for e.g. "12.15.1"
+    - `customicon` - custom icon to show on the GUI dialog
+    - `heading` - the heading to show on the GUI dialog
+    - `message` - the message to show on the GUI dialog
+
+    Note: Alternatively, can set these as script parameters in Jamf Pro
+
+## Jamf Usage
+1. Create a policy that runs *Tap*
+2. Set the policy's frequency to "Once every day"
+    - Adjust the policy's "Run at" time to your liking
+3. Set scope to the Smart group you want to encourage update
+4. Profit
+
+## User Experience
+1. If the user's macOS version is not the targeted version, the user will be prompted to update to the targeted version. 
+
+2. The user will be able to defer the update 3x times. After 3 deferrals, the user will be prompted to update without the option to defer.
+
+3. The user is instructed to close/save any work, and after 10 minutes, Software Update will automatically open, with a dialog that tells the user select update/Restart.
+
+This will occur every day until the user updates to the targeted version.
+
+<img src="./screenshots/guide_custom.png" width="50%">
+
+## Note: Enforcing updates
+Usually not needed because of *Tap* but for those users who needs an extra push or not local admin, you can send APNs remote commands or a policy to force the user to update and restart the compuster. [instant-checkin](https://github.com/amadotejada/instant-checkin) could also be useful for other enforcing solutions.
+
+- DEP enrolled machines - MDM [remote commands](https://docs.jamf.com/best-practice-workflows/jamf-pro/managing-macos-updates/Updating_macOS_Using_a_Mass_Action.html)
+
+- non-DEP enrolled machines - MDM [policy](https://docs.jamf.com/best-practice-workflows/jamf-pro/managing-macos-updates/Running_Software_Update_Using_a_Policy.html)
+
+
+##
+### License
+
+*Tap* is released under the [MIT License](https://github.com/amadotejada/TapmacOS/blob/main/LICENSE)
+
+####
---
+++ tap_updates.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+# Jamf policy script parameters, e.g. below:
+# [ "$4" != "" ] && [ "$targetosverion" == "" ] && [ "$targetosverion" == "$4" ]
+# [ "$5" != "" ] && [ "$targetosverion" == "" ] && [ "$targetosverion" == "$5" ]
+# targetosverion="$4"
+# macosname="$5"
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+plist_path=/usr/local/."defer_update.plist"
+if [[ ! -f "$plist_path" ]]; then
+  /usr/libexec/PlistBuddy -c "Add :deferred integer 3" "$plist_path"
+fi
+
+user=$(id -P $(stat -f%Su /dev/console) | cut -d : -f 8 | cut -d " " -f1)
+serial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+model=$(sysctl hw.model | awk '/hw.model:/ {print $2}')
+osversion=$(sw_vers -productVersion)
+platform_arch=$(/usr/bin/arch)
+jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+plist_var="$(/usr/libexec/PlistBuddy -c 'print ":deferred"' $plist_path)"
+power_source=$(pmset -g batt | head -n 1 | cut -d \' -f2)
+CURRENT_USER=$(/usr/bin/stat -f%Su "/dev/console")
+USER_ID=$(/usr/bin/id -u "$CURRENT_USER")
+
+# set the variables as needed
+###################################################################################################
+customicon="/usr/local/.ventura_icon.png"
+macosname="Ventura"
+targetosverion="12.5.12"
+description=$(echo"$message")
+heading="Update Now  macOS "$macosname" "$targetosverion""
+defer_button="Defer Update"
+update_button="Update Now"
+message="
+Hello "$user",
+                                                          
+Your computer requires a mandatory update to macOS "$macosname" "$targetosverion"
+                                                          
+Model: "$model"
+Serial: "$serial"
+Computer Platform: "$platform_arch"
+Current macOS: "$osversion"
+                                                          
+- IT ❤️
+                                                          
+-----------------------------------------------------------------------------
+                                                          
+Note: This process takes up to 30 minutes or more to complete
+                                                          
+⚠️ Please save any unsaved work - "$plist_var" deferrals remaining ⚠️"
+##############################################################################################
+
+echo "Power Source: $power_source"
+echo "Platform: $platform_arch"
+echo "Running: macOS $osversion"
+echo "Targeting: macOS $targetosverion"
+
+if [[ ! -f $customicon ]]; then
+  echo "customicon not found"
+  customicon="/System/Library/PreferencePanes/SoftwareUpdate.prefPane/Contents/Resources/SoftwareUpdate.icns"
+fi
+
+if [[ ${osversion} = "$targetosverion" ]] || [[ ${osversion} > "$targetosverion" ]]; then
+  rm -f "$plist_path"
+fi
+
+check_jamfhelper() {
+  if [[ ! -f "/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" ]]; then
+    echo "jamfHelper not found, exiting..."
+    exit 1
+  fi
+}
+
+defer_check() {
+  if [[ "$plist_var" = 0 ]]; then
+    if [[ ${osversion} != "$targetosverion" ]] && [[ ${osversion} < "$targetosverion" ]] && [[ -z $(ps axco pid,command | grep "com.apple.preferences.softwareupdate.remoteservice") ]]; then
+      sudo ps axco pid,command | grep jamfHelper | awk '{ print $1; }' | xargs sudo kill -9
+      echo "${osversion} != "$targetosverion" - showing dialog - 0 deferral"
+      if [[ "$platform_arch" = "i386" ]]; then
+        check=$("$jamfHelper" -windowType utility -lockHUD -title "$title" -description "$message" -alignDescription center \
+          -button1 "$update_button" -icon "$customicon" -iconSize 128 -heading "$heading" -alignHeading center -timeout 600 -countdown -alignCountdown right -defaultButton 1)
+      else
+        check=$("$jamfHelper" -windowType utility -lockHUD -title "$heading" -description "$message" \
+          -button1 "$update_button" -icon "$customicon" -iconSize 128 -timeout 600 -countdown -alignCountdown right -defaultButton 1)
+      fi
+      if [[ ${check} -eq 0 ]] && [[ ${check} != "" ]]; then
+        run_updates
+      fi
+    else
+      echo "Skipping - osversion "$osversion" >= targetosverion "$targetosverion" or systempref software update already open"
+      exit 0
+    fi
+  else
+    defer_jamfhelper
+  fi
+}
+
+defer_jamfhelper() {
+  if [[ "$power_source" != "Battery Power" ]]; then
+    if [[ ${osversion} != "" ]] && [[ "${targetosverion}" != "" ]]; then
+      if [[ -f "$plist_path" ]]; then
+        if [[ ${osversion} != "$targetosverion" ]] && [[ ${osversion} < "$targetosverion" ]] && [[ -z $(ps axco pid,command | grep "com.apple.preferences.softwareupdate.remoteservice") ]]; then
+          echo "${osversion} != ${targetosverion} - showing dialog"
+          sudo ps axco pid,command | grep jamfHelper | awk '{ print $1; }' | xargs sudo kill -9
+          if [[ "$platform_arch" = "i386" ]]; then
+            check=$("$jamfHelper" -windowType utility -description "$message" -alignDescription center \
+              -button1 "$update_button" -button2 "$defer_button" -icon "$customicon" -iconSize 128 -heading "$heading" -alignHeading center -defaultButton 1)
+          else
+            check=$("$jamfHelper" -windowType utility -lockHUD -title "$heading" -description "$message" \
+              -button1 "$update_button" -button2 "$defer_button" -icon "$customicon" -iconSize 128 -defaultButton 1)
+          fi
+          if [[ ${check} -eq 0 ]] && [[ ${check} != "" ]]; then
+            echo "Opening System Prefs..."
+            run_updates
+          elif [[ ${check} -eq 2 ]] && [[ ${check} != "" ]]; then
+            echo "Update: deferred"
+            defer_plist
+          else
+            echo "Update: cancelled"
+          fi
+        else
+          echo "Skipping - osversion "$osversion" >= targetosverion "$targetosverion" or systempref software update already open"
+          exit 0
+        fi
+      else
+        if [[ ${osversion} != "$targetosverion" ]] && [[ ${osversion} < "$targetosverion" ]] && [[ -z $(ps axco pid,command | grep "com.apple.preferences.softwareupdate.remoteservice") ]]; then
+          run_updates
+        else
+          echo "Skipping - osversion "$osversion" >= targetosverion "$targetosverion" or systempref software update already open"
+          exit 0
+        fi
+      fi
+    fi
+  fi
+}
+
+defer_plist() {
+  defer_count=$(/usr/libexec/PlistBuddy -c 'print ":deferred"' "$plist_path")
+  if [[ "$defer_count" -gt 2 ]]; then
+    /usr/libexec/PlistBuddy -c "set :deferred 2" $plist_path
+  elif [[ "$defer_count" == 2 ]]; then
+    /usr/libexec/PlistBuddy -c "set :deferred 1" $plist_path
+  elif [[ "$defer_count" == 1 ]]; then
+    /usr/libexec/PlistBuddy -c "set :deferred 0" $plist_path
+  fi
+  echo "Plist: defers remaining $(/usr/libexec/PlistBuddy -c 'print ":deferred"' $plist_path)"
+}
+
+run_updates() {
+  if /usr/bin/fdesetup status | /usr/bin/grep -q "in progress"; then
+    echo "❌ ERROR: FileVault encryption or decryption is in progress."
+    exit 1
+  fi
+
+  if nc -zw1 swscan.apple.com 443; then
+    echo "Connected to Apple servers, continuing"
+  else
+    echo "❌ ERROR: No connection to the Internet."
+    exit 1
+  fi
+
+  heading="Update Now  macOS "$macosname" "$targetosverion""
+  message="- Select Update/Upgrade
+- Then select Restart
+
+Note: Stay connected to power source"
+  echo "Prompting System Preferences > Software Update..."
+  open "/System/Library/PreferencePanes/SoftwareUpdate.prefPane"
+  sleep 1
+  dialog=$("$jamfHelper" -windowType utility -windowPosition "ur" -icon "$customicon" -title "$heading" -description "$message" -timeout 15)
+  exit 0
+}
+
+main() {
+  if nc -zw1 swscan.apple.com 443; then
+    if [[ "$power_source" != "Battery Power" ]]; then
+      check_jamfhelper
+      defer_check
+    else
+      echo 'Power Source: Battery Power - Must be connected to Power Source to run updates'
+      exit 1
+    fi
+  else
+    echo "❌ ERROR: No connection to the Internet."
+  fi
+}
+
+main
